4efed3f35e740d444cd8690f7356fd77d1d347ce
Jun Choi <cjonyou@naver.com>
2025-11-10 13:16:29 +0900

    분석 실행 cmd 수정(2025-11-10 13:15)

--- commands/1-2-1.자바재분석-fns.bat
+++ commands/1-2-1.자바재분석-fns.bat
@@ -1,6 +1,6 @@
 cd .. 
 call .venv\Scripts\activate
-cmd /c "python -m csa.cli.main analyze --java-object --clean --project-name sml-fns-online --application-name SML"
+cmd /c "python -m csa.cli.main analyze --java-source-folder "target_src/sml-fns-online" --java-object --clean --project-name sml-fns-online --application-name SML"
 cd commands
 echo [Current Directory] : %cd%
 pause
--- commands/1-2-2.자바재분석-pds.bat
+++ commands/1-2-2.자바재분석-pds.bat
@@ -1,6 +1,6 @@
 cd .. 
 call .venv\Scripts\activate
-cmd /c "python -m csa.cli.main analyze --java-object --clean --project-name sml-pds-online --application-name SML"
+cmd /c "python -m csa.cli.main analyze --java-source-folder "target_src/sml-pds-online" --java-object --clean --project-name sml-pds-online --application-name SML"
 cd commands
 echo [Current Directory] : %cd%
 pause
